59cd50b66c71699ccb2163451682881db556d107
Nicole Casares <nicolecasares@gmail.com>
2020-10-14 22:16:11 -0700

    added showpage and create element function

--- js/script.js
+++ js/script.js
@@ -13,11 +13,48 @@ For assistance:
 
 
 
+
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
+// function createElement(elementName, property, value) {
+//    const element = document.createElement(elementName);
+//    element[property] = value;
+//   }
+
+const showPage = (list, page) => { 
+  const start = (page * 9) - 9;
+  const end = page * 9;
+  const ul =  document.querySelector('.student-list');
+  ul.innerHTML = '';
+
+  for(let i = 0; list.length < 10; i++) {
+      if(list[i] >= start && list[i] < end) {
+
+         const createElement = (elementName, property, value) => {
+            const element = document.createElement(elementName);
+            element[property] = value;
+         }
+
+         // Creating all the elements for each student
+         const li = createElement(li, className, 'student-item cf');
+         const detailContainer = createElement(div, className, 'student-details');
+         const profilePic = createElement(img, className, 'avatar');
+            profilePic.src = '';
+            profilePic.alt = ''; 
+         const studentName = createElement(h3, className, 'email');
+         const studentEmail = createElement(span, className, 'joined-details');
+         const joinedContainer = createElement(div, className, 'joined-details'); 
+         const joindDate = createElement(span, className, 'date');
+         
+
+
+      }
+  }
+
+};
 
 
 /*
